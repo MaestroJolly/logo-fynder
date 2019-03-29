c693332d4eb2daf2f71bc1a9ffb041256a09df41
unknown <jolaosoyusuf@gmail.com>
2019-03-29 14:00:41 +0100

    connecting fetch-logo endpoint to the frontend using ajax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,41 @@
-var form, companyLink, grayScale, logoSize, content;
+var form, companyLink, greyScale, logoSize, content, logoResult, timeout;
 
 form = $("form");
 companyLink = $("#company-link");
-grayScale = $("#gray-scale");
+greyScale = $("#greyscale");
 logoSize = $("#logo-size");
+logoResult = $("#logo-result");
 content = $("#content");
+timeout = 1000;
 
 form.submit(function(event){
+    event.preventDefault();
     if (!content.hasClass("result-animation")){
-        content.height("50%");
         content.toggleClass("result-animation");
+        content.height("50%");
+        logoResult.css("display", "block");
+        setTimeout(function(){
+            $.ajax({
+                type: "POST",
+                url: "/fetch-logo",
+                data: {
+                    link: companyLink.val(),
+                    size: logoSize.val(),
+                    greyscale: greyScale.prop("checked")
+                },
+                success: function(data){
+                    // console.log(data);
+                    console.log("worked");
+                    logoResult.html(data);
+                    console.log(typeof data);
+                },
+                error: function(error){
+                    console.log(error);
+                }
+            })
+        }, timeout);
     }
     console.log(companyLink.val());
-    console.log(grayScale.val());
+    console.log(greyScale.prop("checked"));
     console.log(logoSize.val());
-    event.preventDefault();
 });
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var request = require('request');
 var bodyParser = require('body-parser')
 var port = process.env.PORT || 3030
 
@@ -14,6 +15,25 @@ app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
     res.render('index')
+})
+
+app.post('/fetch-logo', function(req, res){
+    console.log(req.body);
+    if (req.body.size != "" || req.body.greyscale === true){
+        request.get('https://logo.clearbit.com/'+req.body.link+"?size="+req.body.size+"&greyscale="+req.body.greyscale, function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log(response.headers['content-type']) // 'image/png'
+            res.send(body);
+        });
+    }else{
+        request.get('https://logo.clearbit.com/'+req.body.link, function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log(response.headers['content-type']) // 'image/png'
+            res.send(body)
+        });
+    }
 })
  
 app.listen(port, err => {
